5f6b6b5b702720a73689b68340436fb16c2f76a0
wilber <wilberjasonmarlon@gmail.com>
2020-08-08 15:24:28 -0400

    all

---
+++ app/src/main/java/cu/wilb3r/bottomnavigationkotlin/MainActivity.kt
@@ -0,0 +1,42 @@
+ package cu.wilb3r.bottomnavigationkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import cu.wilb3r.bottomnavigationkotlin.databinding.ActivityMainBinding
+import cu.wilb3r.bottomnavigationkotlin.ui.BuysFragment
+import cu.wilb3r.bottomnavigationkotlin.ui.HomeFragment
+import cu.wilb3r.bottomnavigationkotlin.ui.ProfileFragment
+
+ class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment(HomeFragment(), "home")
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    setFragment(HomeFragment(), "home")
+                }
+                R.id.menu_buy -> {
+                    setFragment(BuysFragment(), "buy")
+                }
+                R.id.menu_profile -> {
+                    setFragment(ProfileFragment(), "profile")
+                }
+            }
+            true
+        }
+    }
+
+     private fun setFragment(fragment: Fragment, tag: String) {
+         supportFragmentManager.beginTransaction().apply {
+             replace(R.id.frgContainer, fragment, tag)
+             commit()
+         }
+     }
+}
---
+++ app/src/main/java/cu/wilb3r/bottomnavigationkotlin/ui/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package cu.wilb3r.bottomnavigationkotlin.ui
+
+import androidx.fragment.app.Fragment
+import cu.wilb3r.bottomnavigationkotlin.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
+
+}
